67fa57548b0b8d50c2704ecc0126a389fbf44550
KirillDolzhenkov <k.dolzhenkov@gmail.com>
2024-12-01 23:46:25 +0300

    refactor: removed temporary files

--- src/app/localStorageMiddleware.ts
+++ src/app/localStorageMiddleware.ts
@@ -1,7 +1,7 @@
 import { Middleware } from '@reduxjs/toolkit';
 
 export const localStorageMiddleware: Middleware = (storeAPI) => (next) => (action) => {
-  const result = next(action); // Передаём действие дальше, чтобы обновить состояние.
+  const result = next(action);
 
   const state = storeAPI.getState();
 
